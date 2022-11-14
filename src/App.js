df6f68a6838ed4243250b5611c6b9c5e79e7869b
Mehul208 <65774892+Mehul208@users.noreply.github.com>
2022-11-14 11:00:29 +0530

    Setup Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import CheckoutPage from "./components/CheckoutPage/checkoutPage";
+import Home from "./components/HomePage/home";
+import ProductDetail from "./components/ProductPage/productDetail";
 
 function App() {
-    return <div className="App">welcome</div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="product/:id" element={<ProductDetail />} />
+                <Route path="checkout" element={<CheckoutPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
