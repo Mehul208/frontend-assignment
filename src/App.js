392915edc12269666be05faed31f9c45a3f5f26b
Mehul208 <65774892+Mehul208@users.noreply.github.com>
2022-11-14 11:26:30 +0530

    Create Navbar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import "./App.css";
 import CheckoutPage from "./components/CheckoutPage/checkoutPage";
 import Home from "./components/HomePage/home";
 import ProductDetail from "./components/ProductPage/productDetail";
-
+import Navbar from "./components/Navbar/navbar";
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="product/:id" element={<ProductDetail />} />
-                <Route path="checkout" element={<CheckoutPage />} />
-            </Routes>
+            <div>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="product/:id" element={<ProductDetail />} />
+                    <Route path="checkout" element={<CheckoutPage />} />
+                </Routes>
+            </div>
         </BrowserRouter>
     );
 }
---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+const Navbar = () => {
+    return (
+        <div className="nvbr">
+            <div className="nv_logo">E-Commerce</div>
+            <div className="nv_nav">
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="#">Cart</Link>
+                    </li>
+                    <li>
+                        <Link to="#">Profile</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
