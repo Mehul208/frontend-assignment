3d9f722e52ee8cce1f968ca84e7c2707be6b05a1
Mehul208 <65774892+Mehul208@users.noreply.github.com>
2022-11-14 13:49:31 +0530

    Added Fake Products Api Mapped Products to Product Card

--- src/components/HomePage/home.jsx
+++ src/components/HomePage/home.jsx
@@ -1,7 +1,36 @@
-import React from "react";
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import DisplayProducts from "./displayProducts";
+import "./style.css";
 const Home = () => {
-    return <div>Home</div>;
+    const [products, setProducts] = useState();
+
+    const getProducts = async () => {
+        try {
+            const { data: res } = await axios.get(
+                "https://fakestoreapi.com/products"
+            );
+            console.log(res);
+            setProducts(res);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    useEffect(() => {
+        if (!products) getProducts();
+    }, [products]);
+
+    return (
+        <div>
+            <div>
+                {products ? (
+                    <DisplayProducts products={products} />
+                ) : (
+                    "loading .."
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default Home;
